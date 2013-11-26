85d069d8b9d02cd3ac902e5fe8303845b692ab88
Paavo <pparkkin@gmail.com>
2013-11-26 20:18:11 +0900

    Add foreach.

--- Protocols/src/main/scala/pparkkin/games/immortals/datatypes/Players.scala
+++ Protocols/src/main/scala/pparkkin/games/immortals/datatypes/Players.scala
@@ -8,11 +8,14 @@ class Players private (val c: Map[String, (Int, Int)]) {
   def size = c.size
   def map[A](f: ((String, (Int, Int))) => A): Iterator[A] =
     c.iterator.map[A](f)
+  def foreach(f: ((String, (Int, Int))) => Unit) {
+    c.foreach(f)
+  }
   def ==(other: Players) =
     this.c.equals(other.c)
 }
 
 object Players {
-  def apply(c: Map[String, (Int, Int)]) = new Players(c)
-  def empty = new Players(Map.empty[String, (Int, Int)])
+  def apply(c: Map[String, (Int, Int)]): Players = new Players(c)
+  def empty: Players = new Players(Map.empty[String, (Int, Int)])
 }
